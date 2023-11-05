b1db669035aa16aa42892d53a5666d836bbe5ee4
NguyenCongNam <namnguyencong23@gmail.com>
2023-11-05 20:45:59 +0700

    setting up socket.io

--- server.js
+++ server.js
@@ -1,33 +1,67 @@
 const app = require("./app");
 
-const dotenv = require("dotenv");
-
 const mongoose = require("mongoose");
 
+const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
+
+const { Server } = require("socket.io");
 
 process.on("uncaughtException", (err) => {
   console.log(err);
-  process.exit(1);
+  process.exit(1); // Exit code 1 indicates that a container shut down
 });
 
 const http = require("http");
 const { error } = require("console");
+const User = require("./models/user");
 
 const server = http.createServer(app);
 
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
 const DB = process.env.DBURI.replace("<PASSWORD>", process.env.DBPASSWORD);
 
-mongoose.connect(DB).then((con) => {
-  console.log("DB connection is successfull");
-}).catch((error)=>{
-  console.log(error)
-});
+mongoose
+  .connect(DB)
+  .then((con) => {
+    console.log("DB connection is successfull");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const port = process.env.PORT || 8000;
 
 server.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+io.on("connection", async (socket) => {
+  const user_id = socket.handshake.query["user_id"];
+
+  const socket_id = socket.id;
+
+  console.log(`User connected ${socket_id}`);
+
+  if (user_id) {
+    await User.findByIdAndUpdate(user_id, { socket_id });
+  }
+
+  //can write our socket event listeners here...
+
+  socket.on("friend_request", async (data) => {
+    console.log(data.to);
+
+    const to = await User.findById(data.to);
+    //TODO => create a friend request
+    io.to(to.socket_id).emit("new_friend_request", {});
+  });
 });
 
 process.on("unhandledRejection", (err) => {
